498d7a9cea9b06478aa7e4c9c6590221f7150c76
Josep Boix Requesens <jspboix@gmail.com>
2025-02-19 11:11:37 +0100

    fix: reduce vpc endpoints to lower costs
    Removed unused and redundant VPC endpoints to cut monitoring system expenses:
    
    - S3: No longer needed.
    - ECR API & ECR DKR: Limited to a single availability zone where ECS runs.

--- pillarbox-monitoring-terraform/20-pillarbox-monitoring-app/main.tf
+++ pillarbox-monitoring-terraform/20-pillarbox-monitoring-app/main.tf
@@ -142,7 +142,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id              = data.aws_vpc.main_vpc.id
   service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = data.aws_subnets.private_subnets.ids
+  subnet_ids          = [data.aws_subnets.private_subnets.ids[0]]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 
@@ -156,25 +156,12 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_id              = data.aws_vpc.main_vpc.id
   service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = data.aws_subnets.private_subnets.ids
+  subnet_ids          = [data.aws_subnets.private_subnets.ids[0]]
   security_group_ids  = [aws_security_group.vpc_endpoints_sg.id]
   private_dns_enabled = true
 
   tags = {
     Name = "vpc-ecr-dkr-endpoint"
-  }
-}
-
-# VPC Endpoint for S3
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = data.aws_vpc.main_vpc.id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
-  vpc_endpoint_type = "Gateway"
-
-  route_table_ids = data.aws_route_tables.private_route_tables.ids
-
-  tags = {
-    Name = "vpc-s3-endpoint"
   }
 }
 

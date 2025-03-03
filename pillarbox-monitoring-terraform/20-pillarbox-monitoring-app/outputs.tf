650efae868cd09d15a9f4524013961795298613d
Josep Boix Requesens <jspboix@gmail.com>
2025-03-03 11:01:18 +0100

    feat: migrate to self-hosted Grafana
    Resolves #23 by moving from AWS Managed Grafana to a self-hosted EC2 instance.
    
    - Reduces costs by eliminating per-user fees, offering a fixed pricing model.
    - Enables full customization and use of our own domain.
    - Simplifies internet access; previously, the managed Grafana was restricted to the VPC.
    - Removes the need for the IL proxy instance.
    - Eliminates VPC endpoints to SNS; Grafana can now handle alerts directly via its own alert manager.

--- pillarbox-monitoring-terraform/20-pillarbox-monitoring-app/outputs.tf
+++ pillarbox-monitoring-terraform/20-pillarbox-monitoring-app/outputs.tf
@@ -1,8 +1,3 @@
-output "grafana_endpoint" {
-  description = "Grafana endpoint"
-  value       = aws_grafana_workspace.grafana_workspace.endpoint
-}
-
 output "opensearch_endpoint" {
   description = "The endpoint of the OpenSearch domain"
   value       = aws_opensearch_domain.opensearch_domain.endpoint
@@ -11,14 +6,4 @@ output "opensearch_endpoint" {
 output "dispatch_dns_name" {
   description = "The DNS name of the Dispatch Service Load Balancer"
   value       = aws_alb.dispatch_alb.dns_name
-}
-
-output "grafana_alert_topic" {
-  description = "The ARN of the topic for grafana alerts"
-  value       = aws_sns_topic.grafana_alerts.arn
-}
-
-output "ec2_proxy_private_ip" {
-  description = "Private IP of the EC2 Nginx Proxy Instance (accessible within VPC)"
-  value       = aws_instance.il_proxy.private_ip
 }
